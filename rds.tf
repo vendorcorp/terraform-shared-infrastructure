830c51e6d5286212f34ba586313615696f2999dd
Paul Horton <phorton@sonatype.com>
2024-02-05 16:27:25 -0500

    fix: support regions that might already have RDS (non VendorCorp)
    Signed-off-by: Paul Horton <phorton@sonatype.com>

--- rds.tf
+++ rds.tf
@@ -3,16 +3,25 @@
 ################################################################################
 data "aws_rds_clusters" "vendorcorp" {}
 
+
 locals {
-  rds_cluster_name = length(data.aws_rds_clusters.vendorcorp.cluster_identifiers) > 0 ? [for c in data.aws_rds_clusters.vendorcorp.cluster_identifiers : c if startswith(c, "vendorcorp-")][0] : null
+  rds_clusters = length(data.aws_rds_clusters.vendorcorp.cluster_identifiers) > 0 ? [for c in data.aws_rds_clusters.vendorcorp.cluster_identifiers : c if startswith(c, "vendorcorp-")] : []
+  
+    # [for c in data.aws_rds_clusters.vendorcorp.cluster_identifiers : c if startswith(c, "vendorcorp-")][0] 
+    # : null
+}
+
+locals {
+  rds_cluster_name = length(local.rds_clusters) > 0 ? local.rds_clusters[0] : ""
 }
 
 data "aws_rds_cluster" "vendorcorp" {
-  count              = length(data.aws_rds_clusters.vendorcorp.cluster_identifiers)
+  count              = length(local.rds_clusters)
   cluster_identifier = local.rds_cluster_name
 }
 
 data "aws_secretsmanager_secrets" "secrets" {
+  count              = length(local.rds_clusters)
   filter {
     name   = "tag-value"
     values = [data.aws_rds_cluster.vendorcorp[0].arn]
@@ -20,8 +29,8 @@ data "aws_secretsmanager_secrets" "secrets" {
 }
 
 data "aws_secretsmanager_secret" "secret" {
-  count     = length(data.aws_secretsmanager_secrets.secrets.arns)
-  arn       = [for i in data.aws_secretsmanager_secrets.secrets.arns : i][0]
+  count     = length(data.aws_secretsmanager_secrets.secrets)
+  arn       = [for i in data.aws_secretsmanager_secrets.secrets[0].arns : i][0]
 }
 
 data "aws_secretsmanager_secret_version" "current" {
